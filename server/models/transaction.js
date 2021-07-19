a8d86d41b92942356c81c4a2e73c3bc3024d4a39
minhhung123 <prohungnguyenminh@gmail.com>
2021-07-19 10:51:40 +0700

    Add model schema for transaction

---
+++ server/models/transaction.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a name for your transaction']
+  },
+  note: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add some note']
+  },
+  amount: {
+    type: Number,
+    required: [true, 'Please add a positive or negative number']
+  },
+  category: {
+    type: String,
+    required: [true, 'Please choose a category']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
