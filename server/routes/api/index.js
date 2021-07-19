9e2349e9e07645d16ac6b18da0b6068a4348c532
minhhung123 <prohungnguyenminh@gmail.com>
2021-07-19 10:42:19 +0700

    create routes for transaction

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,4 +3,7 @@ const router = require("express").Router();
 // authentication api
 router.use("/auth", require("./auth"));
 
+// transaction api
+router.use("/", require("./transaction"));
+
 module.exports = router;
