855eb922e9f6ea84106d56fbae8ce76c7d42c8e4
hoangminh4444 <lehoangminh.ams@gmail.com>
2021-07-20 18:26:49 +0700

    FORM

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import checkTokenExpire from "./utils/checkTokenExpire";
 import store from "./store";
-
+import TransactionForm from "./components/TransactionForm";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
 import Home from "./components/home";
@@ -17,6 +17,7 @@ function App() {
       <div className="App">
         <Route exact path="/auth/register" component={Register} />
         <Route exact path="/auth/login" component={Login} />
+        <Route exact path="/test/TransactionForm" component={TransactionForm} />
         <Switch>
           <PrivateRoute exact path="/home" component={Home} />
         </Switch>
