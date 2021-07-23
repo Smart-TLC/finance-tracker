614d1f7e77757f0c399830c6073be9f3b5c21ced
Trungsherlock <trungnguyenquochn2202@gmail.com>
2021-07-23 12:33:41 +0700

    trang chinh

--- client/src/components/TransactionList/TransactionListItem.js
+++ client/src/components/TransactionList/TransactionListItem.js
@@ -1,28 +1,68 @@
-import React from 'react'
+import React, {useState} from 'react';
+import clsx from 'clsx';
 import { 
     Grid,
-    Paper,
+    Card,
+    CardContent,
+    CardActions, 
+    Collapse,
+    Typography,
+    IconButton,
 } from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
+    root: {
+        maxWidth: 700,
+      },
+      expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+          duration: theme.transitions.duration.shortest,
+        }),
+      },
+      expandOpen: {
+        transform: 'rotate(180deg)',
       },
 }));
 
 export default function TransactionListItem(props) {
     const {id, name, category, date, money, note} = props.transList;
     const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
 
     return (
             <Grid item key={id} xs={9}>
-                <Paper className={classes.paper}>
-                    {name}
-                    {category}
-                </Paper>
+                <Card className={classes.root}>
+                    <CardContent>
+                        <Typography variant="body1" color="textPrimary">
+                            {name}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" button onClick={() => {window.location.pathname = `/category/${category}`}}>
+                            {category}
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton 
+                            className={clsx(classes.expand, {
+                                [classes.expandOpen]: expanded,
+                              })}
+                              onClick={() => {setExpanded(!expanded)}}
+                              aria-expanded={expanded}
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>
+                                {note}
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
             </Grid>
     )
 }
