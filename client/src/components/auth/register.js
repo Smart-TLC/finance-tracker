dd9eb3913047038afb4458c32355e4b240264d58
hoangminh4444 <lehoangminh.ams@gmail.com>
2021-07-30 11:16:38 +0700

    UI login

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -44,6 +44,7 @@ const useStyles = makeStyles((theme) => ({
     position:"fixed",
     left:0,
     top:0,
+    maxWidth: "100%",
     width:"100%",
     height:"100%",
     backgroundColor: theme.palette.primary.main
@@ -51,7 +52,7 @@ const useStyles = makeStyles((theme) => ({
   form: {
     display: "flex",
     textAlign: "center",
-    border: "1px solid black",
+
     borderRadius: 10,
     boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     backgroundColor: "white",
@@ -201,11 +202,6 @@ const Register = () => {
     </Container>
   );
 };
-// const mapStateToProps = (state) => ({
-//   auth: state.auth,
-//   errors: state.errors,
-// });
 
-// export default connect(mapStateToProps, { registerUser })(Register);
 
 export default Register;
